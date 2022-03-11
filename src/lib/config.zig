a4360d62c9be27830b895a84e371b680c713c580
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2022-03-11 17:32:30 +0000

    Config format got a nice writer

--- src/lib/config.zig
+++ src/lib/config.zig
@@ -1,22 +1,63 @@
 const std = @import("std");
 
 pub const ConfigParser = struct {
-    root: ConfigNode,
+    const children_type = std.StringArrayHashMap(Title);
+    children: children_type,
+    alloc: std.mem.Allocator,
 
     pub fn new(alloc: std.mem.Allocator) @This() {
         return .{
-            .root = ConfigNode.new(),
+            .children = children_type.init(alloc),
+            .alloc = alloc,
         };
+    }
+    pub fn get_strict(this: *@This(), key: []const u8) ?*Title {
+        return this.children.getPtr(key);
+    }
+    pub fn add(this: *@This(), key: []const u8) ?*Title {
+        if (this.children.contains(key)) {
+            return null;
+        } else {
+            var title = Title.new(this.alloc);
+            this.children.put(key, title) catch {};
+            return this.children.getPtr(key);
+        }
+    }
+    pub fn get(this: *@This(), key: []const u8) *Title {
+        if (this.children.contains(key)) {
+            return this.get_strict(key).?;
+        } else {
+            return this.add(key).?;
+        }
+    }
+
+    pub fn write(this: *@This(), writer: anytype) void {
+        for (this.children.keys()) |t| {
+            writer.print("[{s}]\n", .{ t }) catch {};
+            this.children.getPtr(t).?.write(writer);
+        }
     }
 };
 
-pub const ConfigNode = struct {
-    const children_type = std.AutoArrayHashMap([]const u8, *ConfigNode);
+pub const Title = struct {
+    const children_type = std.StringArrayHashMap([]const u8);
     children: children_type,
 
     pub fn new(alloc: std.mem.Allocator) @This() {
         return .{
             .children = children_type.init(alloc),
         };
+    }
+    pub fn get(this: *@This(), key: []const u8) ?[]const u8 {
+        return this.children.get(key);
+    }
+    pub fn set(this: *@This(), key: []const u8, val: []const u8) void {
+        this.children.put(key, val) catch {};
+    }
+    pub fn write(this: *@This(), writer: anytype) void {
+        for (this.children.keys()) |t| {
+            writer.print("{s} = \"{s}\" \n", .{ t, this.get(t).? }) catch {};
+        }
+        writer.writeByte('\n') catch {};
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
 const clap = @import("clap");
+const lib = @import("lib/lib.zig");
+
+const config = lib.config;
 
 const debug = std.debug;
 const io = std.io;
 
 pub fn main() anyerror!void {
+    var parser = config.ConfigParser.new(std.heap.ArenaAllocator.init(std.heap.page_allocator).allocator());
+    parser.get("Vêtements").set("Chaussure", "Sôssure");
+    parser.get("Vêtements").set("Haut", "Veston trécher");
+    parser.get("Accessoires").set("Sakamain", "Pochette élégante");
+    parser.write(std.io.getStdOut().writer());
+    _ = parser;
     const params = comptime [_]clap.Param(clap.Help){
         clap.parseParam("commit     Commit all changes") catch unreachable,
     };
